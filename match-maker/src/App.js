9cfe4c59bb5cf2b816725ac6ae0ac55628d9dfa5
Muhsin310302 <Muhsin310302@hotmail.com>
2023-08-11 18:18:20 +0200

    V1

---
+++ match-maker/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Match Maker</h1>
+     <button>New game</button>
+    </div>
+  );
+}
+
+export default App;
